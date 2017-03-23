03ae1d1d60d8a16504a3cef08e76b1a850139150
vladimirbelinski <vladimir_belinski@hotmail.com>
2017-03-22 22:37:53 -0300

    Done an initial resolution

--- Trabalho 1/main.py
+++ Trabalho 1/main.py
@@ -1,7 +1,20 @@
-from bottle import route, run, template
+from bottle import run, get, post, view, request, redirect, route, static_file
+import bottle
 
-@route('/hello/<name>')
-def index(name):
-    return template('<b>Hello {{name}}</b>!', name=name)
+chat_content = []
+
+@bottle.route('/static/<path:path>')
+def server_static(path):
+    return static_file(path, root='static')
+
+@get('/chat')
+@post('/chat')
+@view('chat')
+def renderChat():
+    name = request.forms.getunicode('name')
+    message = request.forms.getunicode('message')
+    if name != None and message != None:
+        chat_content.append([name, message])
+    return {'name': name, 'chat_content': chat_content}
 
 run(host='localhost', port=8080)
